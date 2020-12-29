2a8dabaf5e3dce01e660549b4cc9aad1e8f5e88b
TRT <thomas@smartbox-solutions.com>
2020-12-29 12:39:30 +0100

    refactor: add identity name

--- src/store/auth/identity.ts
+++ src/store/auth/identity.ts
@@ -11,12 +11,18 @@ export default class Identity {
   private readonly _idToken: string;
   private readonly _idTokenClaims: object;
   private readonly _expiresOn: Date | null;
+  private readonly _name: string | undefined;
 
   constructor(tokenResponse: AuthenticationResult) {
     this._scopes = tokenResponse.scopes
     this. _idToken = tokenResponse.idToken
     this._idTokenClaims = tokenResponse.idTokenClaims
     this._expiresOn = tokenResponse.expiresOn
+    this._name = tokenResponse.account?.name
+  }
+
+  get name(): string | undefined {
+    return this._name
   }
 
   get scopes(): Array<string> {
